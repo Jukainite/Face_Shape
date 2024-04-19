79dc640e97e5c511a910564f669e9d11ed4b79c9
Jukainite <114989558+Jukainite@users.noreply.github.com>
2024-04-19 22:31:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 import cv2
 import torch
 import torch.nn as nn
@@ -10,7 +11,7 @@ import numpy as np
 import time
 # Load lại mô hình đã được huấn luyện
 model_path = r"face_shape_classifier.pth"
-device = torch.device('cpu')  # Sử dụng CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = torchvision.models.efficientnet_b4(pretrained=False)
 num_classes = 5
 model.classifier = nn.Sequential(
@@ -74,7 +75,7 @@ def predict_from_image(image):
         # Lấy chỉ số có giá trị lớn nhất là nhãn dự đoán
         predicted_class_idx = torch.argmax(output).item()
 
-        train_dataset = {0: 'Heart', 1: 'Oblong', 2: 'Oval', 3: 'Round', 4: 'Square'}
+        train_dataset = {0: 'Khuôn mặt trái tim', 1: 'Khuôn mặt hình chữ nhật/Khuôn mặt dài', 2: 'Khuôn mặt trái xoan', 3: 'Khuôn mặt tròn', 4: 'Khuôn mặt vuông'}
         # Lấy tên của nhãn dự đoán từ tập dữ liệu
         predicted_label = train_dataset[predicted_class_idx]
 
@@ -119,7 +120,7 @@ def predict_from_webcam():
 
                     # Lấy chỉ số có giá trị lớn nhất là nhãn dự đoán
                     predicted_class_idx = torch.argmax(output).item()
-                    train_dataset = {0: 'Heart', 1: 'Oblong', 2: 'Oval', 3: 'Round', 4: 'Square'}
+                    train_dataset = {0: 'Khuôn mặt trái tim', 1: 'Khuôn mặt hình chữ nhật/Khuôn mặt dài', 2: 'Khuôn mặt trái xoan', 3: 'Khuôn mặt tròn', 4: 'Khuôn mặt vuông'}
                     # Lấy tên của nhãn dự đoán từ tập dữ liệu
                     predicted_label = train_dataset[predicted_class_idx]
                     cv2.putText(frame, predicted_label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
@@ -152,25 +153,50 @@ def predict_from_webcam():
 
     return most_common_label
 
-
+class_info = {
+    'Khuôn mặt trái xoan': {
+        'description': 'Những người có khuôn mặt hình trái xoan không bao giờ sai lời nói. Họ luôn biết dùng từ ngữ phù hợp trong mọi tình huống – nghiêm túc hay vui vẻ. Mọi người tôn trọng họ về cách ăn nói và họ cũng có thể hòa hợp với các nhóm tuổi khác nhau nhờ kỹ năng giao tiếp hiệu quả. Đôi khi họ có thể quá tập trung vào việc nói tất cả những điều đúng đắn, điều này có thể khiến họ mất đi những cuộc trò chuyện không được lọc và những khoảnh khắc gắn kết',
+        'careers': ['Truyền thông và Quảng cáo', 'Nghệ thuật và Văn hóa', 'Giáo dục và Đào tạo']
+    },
+    'Khuôn mặt trái tim': {
+        'description': 'Những người có khuôn mặt hình trái tim là người có tinh thần mạnh mẽ. Đôi khi họ có thể quá bướng bỉnh, chỉ muốn mọi việc được thực hiện theo một cách cụ thể. Về mặt tích cực, họ lắng nghe trực giác của mình, điều này bảo vệ họ khỏi rơi vào những tình huống nguy hiểm. Họ cũng rất sáng tạo trong bất cứ điều gì họ làm.',
+        'careers': ['Kinh doanh và Quản lý', 'Nghệ thuật và Sáng tạo']
+    },
+    'Khuôn mặt hình chữ nhật/Khuôn mặt dài': {
+        'description': 'Bạn đã bao giờ nghe nói về việc đọc khuôn mặt và lòng bàn tay chưa? Vâng, ngay cả hình dạng khuôn mặt cũng có thể tiết lộ rất nhiều điều về tính cách của bạn. Nếu bạn có khuôn mặt hình chữ nhật, bạn tin tưởng nhiều vào suy nghĩ. Bạn dành thời gian suy nghĩ trước khi đưa ra bất kỳ quyết định quan trọng nào. Kết quả là bạn có thể suy nghĩ quá nhiều.',
+        'careers': ['Luật sư và Pháp luật', 'Nghiên cứu và Phát triển', 'Tài chính và Đầu tư']
+    },
+    'Khuôn mặt tròn': {
+        'description': 'Những người có khuôn mặt tròn là những người có trái tim nhân hậu. Họ tin vào việc giúp đỡ người khác và làm từ thiện. Do có tấm lòng bao dung nên đôi khi họ không ưu tiên bản thân mình, điều này có thể dẫn đến những kết quả không mấy tốt đẹp cho bản thân họ',
+        'careers': ['Y tế và Chăm sóc sức khỏe', 'Tình nguyện và Cứu trợ', 'Tình nguyện và Cứu trợ']
+    },
+    'Khuôn mặt vuông': {
+        'description': 'Những người có khuôn mặt này thường khá mạnh mẽ - cả về thể chất cũng như tình cảm. Tuy nhiên, hãy đảm bảo rằng bạn tiếp tục nuôi dưỡng những điểm mạnh của mình, nếu không chúng sẽ chỉ ở mức bề nổi trong tương lai.',
+        'careers': ['Xây dựng và Bất động sản', 'Thể thao và Thể dục', 'Kinh doanh và Quản lý']
+    }
+}
 # Định nghĩa giao diện Streamlit
 def main():
     st.title("Face Shape Prediction")
 
-    # Lựa chọn phương pháp dự đoán
-    prediction_method = st.radio("Choose prediction method:", ("Webcam", "Image"))
+    image_file = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'])
+    if image_file is not None:
+        image = Image.open(image_file)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
 
-    if prediction_method == "Webcam":
-        st.write("Press 'q' to stop the webcam prediction after 10 seconds.")
-        most_common_label = predict_from_webcam()
-        st.write("Most common label:", most_common_label)
-    else:
-        image_file = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'])
-        if image_file is not None:
-            image = Image.open(image_file)
+        if st.button('Start'):
             predicted_label = predict_from_image(image)
-            st.write("Predicted label:", predicted_label)
 
+            st.subheader("Hình Dạng Khuôn mặt:")
+            st.markdown(
+                f"<p style='text-align:center; font-size:60px; color:blue'><strong>{predicted_label}</strong></p>",
+                unsafe_allow_html=True)
+
+            st.markdown('**Ngành Nghề Phù Hợp:**')
+            for career in class_info[predicted_label]['careers']:
+                st.markdown(f"- {career}")
+            st.markdown('**Đặc điểm tính cách:**')
+            st.write("Để xem lí giải cụ thể, bạn hãy đăng kí gói vip của thần số học ! ♥ ♥ ♥")
 
 if __name__ == "__main__":
     main()
