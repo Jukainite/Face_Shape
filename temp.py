c701809dd1fe1320ce2ba5c73361248dc4227a97
Jukainite <114989558+Jukainite@users.noreply.github.com>
2024-04-19 22:41:30 +0700

    Update temp.py

--- temp.py
+++ temp.py
@@ -11,7 +11,7 @@ from collections import Counter
 import numpy as np
 import time
 # Load lại mô hình đã được huấn luyện
-model_path = r"D:\Final1\boi\Facial crop\face_shape_classifier.pth"
+model_path = r"face_shape_classifier.pth"
 train_dataset = {0: 'Heart', 1: 'Oblong', 2: 'Oval', 3: 'Round', 4: 'Square'}
 
 class MyNormalize(object):
