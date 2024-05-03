e888ad69f43e7f3feec8bdd0e250619726509d17
Jukainite <114989558+Jukainite@users.noreply.github.com>
2024-05-03 20:25:21 +0700

    Update temp2.py

--- temp2.py
+++ temp2.py
@@ -238,6 +238,7 @@ def main():
     # ctx = webrtc_streamer(key="snapshot", client_settings=WEBRTC_CLIENT_SETTINGS,video_processor_factory=VideoTransformer)
     rtc_configuration = {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
     account_sid = os.environ['ACc4f7b8e2ac4c15f6ba35d671cc8af7e7']
+    
     auth_token = os.environ['8f0500d3d531fcd5a0ee80ab31b21926']
     client = Client(account_sid, auth_token)
     
