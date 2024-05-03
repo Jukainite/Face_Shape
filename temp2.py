17be2d13e25bacb280524946ec5edc6cb6113eec
Jukainite <114989558+Jukainite@users.noreply.github.com>
2024-05-03 21:50:09 +0700

    Update temp2.py

--- temp2.py
+++ temp2.py
@@ -32,12 +32,7 @@ def get_ice_servers():
     """
 
     # Ref: https://www.twilio.com/docs/stun-turn/api
-    load_dotenv()
-    env_file = os.getenv('GITHUB_ENV')
-
-    with open(env_file, "a") as myfile:
-        myfile.write("TWILIO_ACCOUNT_SID=ACc4f7b8e2ac4c15f6ba35d671cc8af7e7\n")
-        myfile.write("TWILIO_AUTH_TOKEN=8f0500d3d531fcd5a0ee80ab31b21926")
+    
     try:
         account_sid = os.environ["TWILIO_ACCOUNT_SID"]
         auth_token = os.environ["TWILIO_AUTH_TOKEN"]
