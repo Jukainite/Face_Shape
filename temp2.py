363f926e00d4eeb56c1456e9723ffe9a2efb4c4a
Jukainite <114989558+Jukainite@users.noreply.github.com>
2024-05-11 16:32:10 +0700

    Update temp2.py

--- temp2.py
+++ temp2.py
@@ -205,7 +205,8 @@ def main():
     st.markdown(
         "Luư Ý Khi sử dụng:"
         " Bạn hãy mở camera và để app xác định khuôn mặt của bạn. Khi phát hiện ra nó sẽ khoanh vùng khuôn mặt. "
-        " Khi khung xuất hiện hãy đợi 1 tí (khoảng 3s xuất hiện khung) rồi nhấn nút predict phía dưới")
+        " Khi khung xuất hiện hãy đợi 1 tí (khoảng 3s xuất hiện khung) rồi nhấn nút predict phía dưới"
+        "Nếu khuôn mặt không được phát hiện, bạn có thể thử chụp hình lại nhiều lần")
 
 
 
