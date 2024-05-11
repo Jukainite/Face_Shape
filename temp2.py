63ea4fd66e292a7316c225224da37422ab491ae2
Jukainite <114989558+Jukainite@users.noreply.github.com>
2024-05-11 15:08:00 +0700

    Update temp2.py

--- temp2.py
+++ temp2.py
@@ -264,7 +264,7 @@ def main():
         mode=WebRtcMode.SENDRECV,
         rtc_configuration={
             "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}],
-            "iceTransportPolicy": "relay",
+            "iceTransportPolicy": "public",
         },
         media_stream_constraints=media_stream_constraints,
         video_processor_factory=VideoTransformer
